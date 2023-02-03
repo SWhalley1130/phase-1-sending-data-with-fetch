6b6ee01a0b65bca5bc9f62c3401c6b55ad53af04
Sarah Whalley <s.whalley1130@gmail.com>
2023-02-03 11:33:20 -0700

    Meets deliverables for lab, but tests do not pass

--- index.js
+++ index.js
@@ -1,1 +1,106 @@
 // Add your code here
+document.addEventListener("DOMContentLoaded", function(){
+    
+    initDogs();
+    function initDogs()
+    {
+        return fetch('http://localhost:3000/dogs')
+        .then(res=>res.json())
+        .then(dogs=>
+        {
+            dogs.forEach(element => 
+            {
+                const li = document.createElement('li');
+                li.textContent=`${element.dogName} is a ${element.dogBreed}`
+                document.querySelector('ul').appendChild(li);   
+            });
+        })
+    }
+
+    const dogSubmitButton=document.querySelector("#dogForm");
+    dogSubmitButton.addEventListener('submit', handleDogSubmit);
+
+    function handleDogSubmit(e)
+    {
+        e.preventDefault();
+        const formData=
+        {
+            dogName:document.querySelector("#dogName").value,
+            dogBreed:document.querySelector("#dogBreed").value
+        }
+
+        return fetch('http://localhost:3000/dogs', 
+        {
+            method:"POST",
+            headers:
+            {
+                "Content-Type": 'application/json',
+                "Accept":"application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    }
+
+/////////////////////////////////////
+// This Section is for the lab
+////////////////////////////////////
+
+    initUsers();
+    function initUsers()
+    {
+        return fetch('http://localhost:3000/users')
+        .then(res=>res.json())
+        .then(users=>
+        {
+            users.forEach(element => 
+            {
+                const li = document.createElement('li');
+                li.textContent=`${element.name}'s email address is ${element.email}. Their ID is ${element.id}`;
+                document.querySelector('#userList').appendChild(li);   
+            });
+        })
+    }
+
+
+
+    
+    document.querySelector('#userForm').addEventListener('submit', submitData)
+
+    function submitData(e)
+    {
+        e.preventDefault();
+        let formData=
+        {
+            name:document.querySelector("#name").value,
+            email: document.querySelector("#email").value
+        }
+
+        return fetch("http://localhost:3000/users", 
+        {
+            method:"POST",
+            headers:
+            {
+                "Content-Type": 'application/json',
+                "Accept":"application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(res=>res.json())
+        .then(user=>
+        {
+            const li = document.createElement('li');
+            li.textContent=`${user.name}'s email address is ${user.email}. Their ID is ${user.id}`
+            document.querySelector('#userList').appendChild(li); 
+        })
+        .catch(function(error)
+        {
+            const alertMessage=document.createElement('div');
+            alertMessage.textContent=error.message;
+            document.querySelector("#alertMessage").appendChild(alertMessage);
+        })
+
+    }
+
+})
