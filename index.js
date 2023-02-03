acab0818e29bf15743bc3db2ae1c511abd12b0f2
Sarah Whalley <s.whalley1130@gmail.com>
2023-02-03 11:58:26 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,106 +1,111 @@
 // Add your code here
-document.addEventListener("DOMContentLoaded", function(){
+//document.addEventListener("DOMContentLoaded", function(){
     
-    initDogs();
-    function initDogs()
-    {
-        return fetch('http://localhost:3000/dogs')
-        .then(res=>res.json())
-        .then(dogs=>
-        {
-            dogs.forEach(element => 
-            {
-                const li = document.createElement('li');
-                li.textContent=`${element.dogName} is a ${element.dogBreed}`
-                document.querySelector('ul').appendChild(li);   
-            });
-        })
-    }
-
-    const dogSubmitButton=document.querySelector("#dogForm");
-    dogSubmitButton.addEventListener('submit', handleDogSubmit);
-
-    function handleDogSubmit(e)
-    {
-        e.preventDefault();
-        const formData=
-        {
-            dogName:document.querySelector("#dogName").value,
-            dogBreed:document.querySelector("#dogBreed").value
-        }
-
-        return fetch('http://localhost:3000/dogs', 
-        {
-            method:"POST",
-            headers:
-            {
-                "Content-Type": 'application/json',
-                "Accept":"application/json"
-            },
-            body: JSON.stringify(formData)
-        })
-        .then(res=>res.json())
-        .then(data=>console.log(data))
-    }
 
 /////////////////////////////////////
 // This Section is for the lab
 ////////////////////////////////////
 
-    initUsers();
-    function initUsers()
+//document.querySelector('#userForm').addEventListener('submit', submitData)
+
+function submitData(name, email)
+{
+    // e.preventDefault();
+    // let formData=
+    // {
+    //     name:document.querySelector("#name").value,
+    //     email: document.querySelector("#email").value
+    // }
+    //e.preventDefault();
+    return fetch("http://localhost:3000/users", 
     {
-        return fetch('http://localhost:3000/users')
-        .then(res=>res.json())
-        .then(users=>
+        method:"POST",
+        headers:
         {
-            users.forEach(element => 
-            {
-                const li = document.createElement('li');
-                li.textContent=`${element.name}'s email address is ${element.email}. Their ID is ${element.id}`;
-                document.querySelector('#userList').appendChild(li);   
-            });
-        })
-    }
-
-
-
-    
-    document.querySelector('#userForm').addEventListener('submit', submitData)
-
-    function submitData(e)
+            "Content-Type": 'application/json',
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({name,email})
+    })
+    .then(res=>res.json())
+    .then(user=>
     {
-        e.preventDefault();
-        let formData=
-        {
-            name:document.querySelector("#name").value,
-            email: document.querySelector("#email").value
-        }
+        const li = document.createElement('li');
+        li.textContent=`${user.name}'s email address is ${user.email}. Their ID is ${user.id}`
+        document.querySelector('#userList').appendChild(li); 
+    })
+    .catch(function(error)
+    {
+        const alertMessage=document.createElement('div');
+        alertMessage.textContent=error.message;
+        document.querySelector("#alertMessage").appendChild(alertMessage);
+    })
 
-        return fetch("http://localhost:3000/users", 
-        {
-            method:"POST",
-            headers:
-            {
-                "Content-Type": 'application/json',
-                "Accept":"application/json"
-            },
-            body: JSON.stringify(formData)
-        })
-        .then(res=>res.json())
-        .then(user=>
-        {
-            const li = document.createElement('li');
-            li.textContent=`${user.name}'s email address is ${user.email}. Their ID is ${user.id}`
-            document.querySelector('#userList').appendChild(li); 
-        })
-        .catch(function(error)
-        {
-            const alertMessage=document.createElement('div');
-            alertMessage.textContent=error.message;
-            document.querySelector("#alertMessage").appendChild(alertMessage);
-        })
+}
 
-    }
+// initUsers();
+// function initUsers()
+// {
+//     return fetch('http://localhost:3000/users')
+//     .then(res=>res.json())
+//     .then(users=>
+//     {
+//         users.forEach(element => 
+//         {
+//             const li = document.createElement('li');
+//             li.textContent=`${element.name}'s email address is ${element.email}. Their ID is ${element.id}`;
+//             document.querySelector('#userList').appendChild(li);   
+//         });
+//     })
+// }
 
-})
+
+// /////////////////////////////////
+// // This part was for funsies
+// ////////////////////////////////
+
+//     initDogs();
+//     function initDogs()
+//     {
+//         return fetch('http://localhost:3000/dogs')
+//         .then(res=>res.json())
+//         .then(dogs=>
+//         {
+//             dogs.forEach(element => 
+//             {
+//                 const li = document.createElement('li');
+//                 li.textContent=`${element.dogName} is a ${element.dogBreed}`
+//                 document.querySelector('ul').appendChild(li);   
+//             });
+//         })
+//     }
+
+//     const dogSubmitButton=document.querySelector("#dogForm");
+//     dogSubmitButton.addEventListener('submit', handleDogSubmit);
+
+//     function handleDogSubmit(e)
+//     {
+//         e.preventDefault();
+//         const formData=
+//         {
+//             dogName:document.querySelector("#dogName").value,
+//             dogBreed:document.querySelector("#dogBreed").value
+//         }
+
+//         return fetch('http://localhost:3000/dogs', 
+//         {
+//             method:"POST",
+//             headers:
+//             {
+//                 "Content-Type": 'application/json',
+//                 "Accept":"application/json"
+//             },
+//             body: JSON.stringify(formData)
+//         })
+//         .then(res=>res.json())
+//         .then(data=>console.log(data))
+//     }
+
+
+
+// })
