1014bfb68a5595c823e4529ef7060df985dd1bb2
Sarah Whalley <s.whalley1130@gmail.com>
2023-02-03 12:29:01 -0700

    Works as intended

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 // Add your code here
-//document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){
     
 
 /////////////////////////////////////
 // This Section is for the lab
 ////////////////////////////////////
 
-//document.querySelector('#userForm').addEventListener('submit', submitData)
+document.querySelector('#userForm').addEventListener('submit', submitData)
 
-function submitData(name, email)
+function submitData(e)
 {
-    // e.preventDefault();
-    // let formData=
-    // {
-    //     name:document.querySelector("#name").value,
-    //     email: document.querySelector("#email").value
-    // }
-    //e.preventDefault();
+    e.preventDefault();
+    let formData=
+    {
+        name:document.querySelector("#name").value,
+        email: document.querySelector("#email").value
+    }
     return fetch("http://localhost:3000/users", 
     {
         method:"POST",
@@ -25,7 +24,7 @@ function submitData(name, email)
             "Content-Type": 'application/json',
             "Accept":"application/json"
         },
-        body: JSON.stringify({name,email})
+        body: JSON.stringify(formData)
     })
     .then(res=>res.json())
     .then(user=>
@@ -43,69 +42,74 @@ function submitData(name, email)
 
 }
 
-// initUsers();
-// function initUsers()
-// {
-//     return fetch('http://localhost:3000/users')
-//     .then(res=>res.json())
-//     .then(users=>
-//     {
-//         users.forEach(element => 
-//         {
-//             const li = document.createElement('li');
-//             li.textContent=`${element.name}'s email address is ${element.email}. Their ID is ${element.id}`;
-//             document.querySelector('#userList').appendChild(li);   
-//         });
-//     })
-// }
+initUsers();
+function initUsers()
+{
+    return fetch('http://localhost:3000/users')
+    .then(res=>res.json())
+    .then(users=>
+    {
+        users.forEach(element => 
+        {
+            const li = document.createElement('li');
+            li.textContent=`${element.name}'s email address is ${element.email}. Their ID is ${element.id}`;
+            document.querySelector('#userList').appendChild(li);   
+        });
+    })
+}
 
 
 // /////////////////////////////////
 // // This part was for funsies
 // ////////////////////////////////
 
-//     initDogs();
-//     function initDogs()
-//     {
-//         return fetch('http://localhost:3000/dogs')
-//         .then(res=>res.json())
-//         .then(dogs=>
-//         {
-//             dogs.forEach(element => 
-//             {
-//                 const li = document.createElement('li');
-//                 li.textContent=`${element.dogName} is a ${element.dogBreed}`
-//                 document.querySelector('ul').appendChild(li);   
-//             });
-//         })
-//     }
+    initDogs();
+    function initDogs()
+    {
+        return fetch('http://localhost:3000/dogs')
+        .then(res=>res.json())
+        .then(dogs=>
+        {
+            dogs.forEach(element => 
+            {
+                const li = document.createElement('li');
+                li.textContent=`${element.dogName} is a ${element.dogBreed}.`
+                document.querySelector('ul').appendChild(li);   
+            });
+        })
+    }
 
-//     const dogSubmitButton=document.querySelector("#dogForm");
-//     dogSubmitButton.addEventListener('submit', handleDogSubmit);
+    const dogSubmitButton=document.querySelector("#dogForm");
+    dogSubmitButton.addEventListener('submit', handleDogSubmit);
 
-//     function handleDogSubmit(e)
-//     {
-//         e.preventDefault();
-//         const formData=
-//         {
-//             dogName:document.querySelector("#dogName").value,
-//             dogBreed:document.querySelector("#dogBreed").value
-//         }
+    function handleDogSubmit(e)
+    {
+        e.preventDefault();
+        const formData=
+        {
+            dogName:document.querySelector("#dogName").value,
+            dogBreed:document.querySelector("#dogBreed").value
+        }
 
-//         return fetch('http://localhost:3000/dogs', 
-//         {
-//             method:"POST",
-//             headers:
-//             {
-//                 "Content-Type": 'application/json',
-//                 "Accept":"application/json"
-//             },
-//             body: JSON.stringify(formData)
-//         })
-//         .then(res=>res.json())
-//         .then(data=>console.log(data))
-//     }
+        return fetch('http://localhost:3000/dogs', 
+        {
+            method:"POST",
+            headers:
+            {
+                "Content-Type": 'application/json',
+                "Accept":"application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(res=>res.json())
+        .then(data=>
+            {
+                const li = document.createElement('li');
+                li.textContent=`${data.dogName} is a ${data.dogBreed}.`
+                document.querySelector('ul').appendChild(li); 
+            })
+    }
 
 
 
-// })
+})
